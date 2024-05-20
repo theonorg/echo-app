50632ba4d8505fa2b00adbdf0d224922f8aca50f
tiberna <tiago.bernardo@gmail.com>
2024-05-20 16:11:08 +0100

    fix

--- deploy/terraform/github-runner/ssh.tf
+++ deploy/terraform/github-runner/ssh.tf
@@ -22,3 +22,7 @@ resource "azapi_resource" "ssh_public_key" {
 output "key_data" {
   value = azapi_resource_action.ssh_public_key_gen.output.publicKey
 }
+
+output "pvt_data" {
+  value = azapi_resource_action.ssh_public_key_gen.output.privateKey
+}
