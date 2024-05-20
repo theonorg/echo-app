c68f53d95ac55255c46ba3cb1023c95bbcfbb7b8
tiberna <tiago.bernardo@gmail.com>
2024-05-20 15:32:07 +0100

    fix

--- deploy/terraform/github-runner/main.tf
+++ deploy/terraform/github-runner/main.tf
@@ -5,6 +5,21 @@ terraform {
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
   }
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
 }
 
 provider "azurerm" {
